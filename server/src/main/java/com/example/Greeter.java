55ae43189e840e81192f51d13ce18081d37dea18
Filipe Nascimento <filipef9@hotmail.com>
2017-04-15 20:41:05 +0000

    Inclusao de comentario javadoc.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+   * @param someone
+   * @return Hello someone value
+   */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
